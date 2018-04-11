cbd07780a2bcc88e700b50006ec7bdef1bb5951c
Gustavo Brunetto <gustavobruneto@gmail.com>
2018-04-11 15:34:22 -0300

    func: treat the expression

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,51 @@
 
-public class App {
-  
-    public static void main(String[] args) throws Exception {
-      
-      TestDataStructure test = new TestDataStructure();
-      test.testAll();
+import java.util.StringTokenizer;
 
-      
-      
+public class App {
+
+  public static void main(String[] args) throws Exception {
+
+    // TestDataStructure test = new TestDataStructure();
+    // test.testAll();
+
+    // Allowed operations
+    String operations = "(^*/+-)";
+
+    // TODO : the expression needs to be read from the user input
+    String inputExpressionString = "10   + (  2      * 3  -     4 ) ^  2  / 4 +   6   * 2";
+
+    // remove blanck
+    String expressionString = RemoveBlancks.removeBlanck(inputExpressionString);
+
+    System.out.println("Expression to evaluate: " + expressionString);
+
+    // Tokenizer
+    StringTokenizer expression = new StringTokenizer(expressionString, "+-*/^()", true);
+
+    // put elements into the stack or queue
+    String token;
+    Float number;
+    while (expression.hasMoreTokens()) {
+        token = expression.nextToken().toString();
+        if (token.equals("(")) {
+            System.out.println("Go to stack  " + token);
+        } else {
+            try {
+                number = Float.valueOf(token);
+                System.out.println("Number    =  " + number);
+            } catch (Exception e) {
+                if (operations.contains(token)) {
+                    System.out.println("Operation =  " + token);
+                } else {
+                    // TODO: throw new Exception here!
+                    System.out.println("Operation not allowed");
+                }
+            }
+        }
+
     }
+
+    
+
   }
-  
+}
